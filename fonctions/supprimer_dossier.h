cc2a039e2b8df8100ab1f1a7e439a8b4e0cd99ed
Ahmet Adam <ahmetadam4@gmail.com>
2019-05-31 17:20:46 +0200

    Header oublié

---
+++ fonctions/supprimer_dossier.h
@@ -0,0 +1,7 @@
+#ifndef SUPPRIMER_DOSSIER_H_INCLUDED
+#define SUPPRIMER_DOSSIER_H_INCLUDED
+#include "../structures/utilisateur.h"
+
+void supprimer_dossier(utilisateur*);
+
+#endif // SUPPRIMER_DOSSIER_H_INCLUDED
