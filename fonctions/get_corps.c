d527b718e125dd1e15595564db2af1c48d052778
Ahmet Adam <ahmetadam4@gmail.com>
2019-06-04 21:53:55 +0200

    Ajout de la fonction get_corps

---
+++ fonctions/get_corps.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "get_chaine.h"
+
+void get_corps(char corps[]) {
+    printf("Entrez le corps de votre message (<= 500 caractères):\n");
+
+    while (get_chaine(corps, 501) != 1) {
+        printf("Ce message est trop long !\n");
+        printf("Entrez le corps de votre message (<= 500 caractères):\n");
+    }
+}
---
+++ fonctions/get_corps.h
@@ -0,0 +1,6 @@
+#ifndef GET_CORPS_H_INCLUDED
+#define GET_CORPS_H_INCLUDED
+
+void get_corps(char[]);
+
+#endif // GET_CORPS_H_INCLUDED
